8b65d707b0a7e16994aaceb25bb2350f6ad2aae3
entropyAMNESTY <entropy.random.squared@gmail.com>
2022-07-18 08:13:55 +0200

    Added Feature Boxes, CTA, Footer and the Mobile Menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
 const input = document.getElementById('link-input');
 const linkForm = document.getElementById('link-form');
 const errMsg = document.getElementById('err-msg');
 
+btn.addEventListener('click', navToggle);
 linkForm.addEventListener('submit', formSubmit);
+
+// Toggle Mobile Menu
+
+function navToggle() {
+    btn.classList.toggle('open');
+    // Flex to Show
+    menu.classList.toggle('flex');
+    // Hidden to be gone
+    menu.classList.toggle('hidden');
+}
 
 // Validate a URL
 function validURL(str) {
